189af3b43e4bbcb703c2213975f4afb657536a95
Hugo <hugones45@gmail.com>
2024-03-01 16:28:21 -0300

    nine

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -57,6 +57,7 @@ const ComputersCanvas = () => {
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
+      className="z-1"
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
